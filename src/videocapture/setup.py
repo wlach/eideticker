7153e4f33a488b87193775c05967b298ecffcf35
William Lachance <wlachance@mozilla.com>
2011-12-07 12:00:01 -0500

    videocapture module now requires numpy

--- src/videocapture/setup.py
+++ src/videocapture/setup.py
@@ -3,6 +3,7 @@ setup(
     name = "videocapture",
     version = "0.1.0",
     packages = find_packages(),
+    install_requires = [ 'numpy' ]
 )
 
 # FIXME: Compile decklink-capture script automatically
