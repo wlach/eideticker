ec05deaf8d2dd5ed5704a384095a6ba830e409dd
William Lachance <wlachance@mozilla.com>
2012-04-05 19:45:57 -0400

    Redo dashboard so you must specify the test name to test
    It doesn't really make sense to test everything anymore, when we have
    tests like the nytimes one which we won't be including in the eideticker
    source tree

--- bin/update-dashboard.py
+++ bin/update-dashboard.py
@@ -72,7 +72,7 @@ def kill_app(dm, appname):
         dm.runCmd(["shell", "echo kill %s | su" % pid])
 
 def main(args=sys.argv[1:]):
-    usage = "usage: %prog [options] <output dir>"
+    usage = "usage: %prog [options] <test> <output dir>"
     parser = optparse.OptionParser(usage)
     parser.add_option("--no-download",
                       action="store_true", dest = "no_download",
@@ -81,14 +81,19 @@ def main(args=sys.argv[1:]):
                       action="store", dest="product",
                       help = "Restrict testing to product (options: %s)" %
                       ", ".join([product["name"] for product in default_products]))
-    parser.add_option("--test", "-t",
-                      action="store", dest="test",
-                      help="Restrict testing to specific test (options: %s)" %
-                      ", ".join([test["name"] for test in default_tests]))
     options, args = parser.parse_args()
 
-    if len(args) != 1:
+    if len(args) != 2:
         parser.error("incorrect number of arguments")
+
+    (testname, outputdir) = args
+
+    testnames = [test["name"] for test in default_tests]
+    if testname not in testnames:
+        print "ERROR: No tests matching '%s' (options: %s)" % (testname, ", ".join(testnames))
+        sys.exit(1)
+    else:
+        test = [test for test in default_tests if test['name'] == testname][0]
 
     products = default_products
     if options.product:
@@ -97,15 +102,7 @@ def main(args=sys.argv[1:]):
             print "ERROR: No products matching '%s'" % options.product
             sys.exit(1)
 
-    tests = default_tests
-    if options.test:
-        tests = [test for test in tests if test['name'] == options.test]
-        if not tests:
-            print "ERROR: No tests matching '%s'" % options.test
-            sys.exit(1)
-
     current_date = time.strftime("%Y-%m-%d")
-    outputdir = args[0]
     datafile = os.path.join(outputdir, 'data.json')
 
     data = NestedDict()
@@ -132,52 +129,51 @@ def main(args=sys.argv[1:]):
             appinfo = { }
             capture_name = "%s (taken on %s)" % (product['name'], current_date)
 
-        for test in tests:
-            # Kill any existing instances of the processes
-            kill_app(dm, product['appname'])
+        # Kill any existing instances of the processes
+        kill_app(dm, product['appname'])
 
-            # Now run the test
-            capture_file = os.path.join(CAPTURE_DIR,
-                                        "%s-%s-%s-%s.zip" % (test['name'],
-                                                             product['name'],
-                                                             appinfo.get('date'),
-                                                             int(time.time())))
-            retval = subprocess.call(["runtest.py", "--name",
-                             capture_name,
-                             "--capture-file", capture_file,
-                             product['appname'], test['path']])
-            if retval != 0:
-                raise Exception("Failed to run test %s for %s" % (test['name'], product['name']))
+        # Now run the test
+        capture_file = os.path.join(CAPTURE_DIR,
+                                    "%s-%s-%s-%s.zip" % (test['name'],
+                                                         product['name'],
+                                                         appinfo.get('date'),
+                                                         int(time.time())))
+        retval = subprocess.call(["runtest.py", "--name",
+                         capture_name,
+                         "--capture-file", capture_file,
+                         product['appname'], test['path']])
+        if retval != 0:
+            raise Exception("Failed to run test %s for %s" % (test['name'], product['name']))
 
-            # Kill app after test complete
-            kill_app(dm, product['appname'])
+        # Kill app after test complete
+        kill_app(dm, product['appname'])
 
-            capture = videocapture.Capture(capture_file)
+        capture = videocapture.Capture(capture_file)
 
-            # video file
-            video_path = os.path.join('videos', 'video-%s.webm' % time.time())
-            video_file = os.path.join(outputdir, video_path)
-            open(video_file, 'w').write(capture.get_video().read())
+        # video file
+        video_path = os.path.join('videos', 'video-%s.webm' % time.time())
+        video_file = os.path.join(outputdir, video_path)
+        open(video_file, 'w').write(capture.get_video().read())
 
-            if test['measure'] == 'fps':
-                framediff_sums = videocapture.get_framediff_sums(capture)
-                num_different_frames = 1 + len([framediff for framediff in framediff_sums if framediff > 250])
-                measure = num_different_frames / capture.length
-            elif test['measure'] == 'checkerboard':
-                measure = videocapture.get_checkerboarding_area_duration(capture)
-            else:
-                print "ERROR: Unknown measure '%s'" % test['measure']
-                sys.exit(1)
+        if test['measure'] == 'fps':
+            framediff_sums = videocapture.get_framediff_sums(capture)
+            num_different_frames = 1 + len([framediff for framediff in framediff_sums if framediff > 250])
+            measure = num_different_frames / capture.length
+        elif test['measure'] == 'checkerboard':
+            measure = videocapture.get_checkerboarding_area_duration(capture)
+        else:
+            print "ERROR: Unknown measure '%s'" % test['measure']
+            sys.exit(1)
 
-            # need to initialize dict for product if not there already
-            if not data[test['name']].get(product['name']):
-                data[test['name']][product['name']] = {}
+        # need to initialize dict for product if not there already
+        if not data[test['name']].get(product['name']):
+            data[test['name']][product['name']] = {}
 
-            if not data[test['name']][product['name']].get(current_date):
-                data[test['name']][product['name']][current_date] = []
-            data[test['name']][product['name']][current_date].append({ test['measure']: measure,
-                                                                   'video': video_path,
-                                                                   'appdate': appinfo.get('date') })
+        if not data[test['name']][product['name']].get(current_date):
+            data[test['name']][product['name']][current_date] = []
+        data[test['name']][product['name']][current_date].append({ test['measure']: measure,
+                                                               'video': video_path,
+                                                               'appdate': appinfo.get('date') })
 
     # Write the data to disk
     open(datafile, 'w').write(json.dumps(data))
