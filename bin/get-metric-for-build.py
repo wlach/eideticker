afa5289fcffecfdc8b80ecc52015e0537a1eff65
William Lachance <wlach@mozilla.com>
2012-04-16 18:30:27 -0700

    Add option to specify number of runs to the get metric command line tool
    Hopefully this will help with bimodal result distributions, where you have
    different factors affecting performance.

--- bin/get-metric-for-build.py
+++ bin/get-metric-for-build.py
@@ -21,6 +21,9 @@ def kill_app(dm, appname):
 def main(args=sys.argv[1:]):
     usage = "usage: %prog <apk of build> <test> <metric>"
     parser = optparse.OptionParser(usage)
+    parser.add_option("--num-runs", action="store",
+                      type = "int", dest = "num_runs",
+                      help = "number of runs (default: 1)")
 
     options, args = parser.parse_args()
 
@@ -33,7 +36,10 @@ def main(args=sys.argv[1:]):
     if metric not in valid_metrics:
         parser.error("bad metric '%s' (valid metrics: %s)" % (metric, ",".join(valid_metrics)))
 
-    # first get the appname from the apk
+    num_runs = 1
+    if options.num_runs:
+        num_runs = options.num_runs
+
     with zipfile.ZipFile(apk) as zip:
         try:
             appname = zip.read('package-name.txt').rstrip()
@@ -46,28 +52,40 @@ def main(args=sys.argv[1:]):
     droid = mozdevice.DroidADB(packageName=appname)
     droid.updateApp(apk)
 
-    # Kill any existing instances of the processes
-    kill_app(droid, appname)
+    captures = []
 
-    # Now run the test
-    capture_file = os.path.join(CAPTURE_DIR,
-                                "metric-test-%s-%s.zip" % (appname,
-                                                  int(time.time())))
+    for i in range(num_runs):
+        # Kill any existing instances of the processes
+        kill_app(droid, appname)
 
-    retval = subprocess.call(["runtest.py", "--capture-file", capture_file,
-                              appname, test])
-    if retval != 0:
-        raise Exception("Failed to run test %s for %s" % (test, appname))
+        # Now run the test
+        capture_file = os.path.join(CAPTURE_DIR,
+                                    "metric-test-%s-%s.zip" % (appname,
+                                                               int(time.time())))
 
-    capture = videocapture.Capture(capture_file)
+        retval = subprocess.call(["runtest.py", "--capture-file", capture_file,
+                                  appname, test])
+        if retval != 0:
+            raise Exception("Failed to run test %s for %s" % (test, appname))
 
-    if metric == 'fps':
-        framediff_sums = videocapture.get_framediff_sums(capture)
-        num_different_frames = 1 + len([framediff for framediff in framediff_sums if framediff > 250])
-        print "Estimated FPS: %s" % (num_different_frames / capture.length)
+        capture = videocapture.Capture(capture_file)
+
+        if metric == 'unique_frames':
+            framediff_sums = videocapture.get_framediff_sums(capture)
+            num_unique_frames = 1 + len([framediff for framediff in framediff_sums if framediff > 0])
+            metric = num_unique_frames
+        elif metric == 'checkerboard':
+            metric = videocapture.get_checkerboarding_area_duration(capture)
+
+        captures.append({'capture_file': capture_file, 'metric': metric})
+
+    if metric == 'unique_frames':
+        print "=== Number of unique frames ==="
     elif metric == 'checkerboard':
-        print "Checkerboard area/duration (sum of percents NOT percentage): %s" % videocapture.get_checkerboarding_area_duration(capture)
+        print "=== Checkerboard area/duration (sum of percents NOT percentage) ==="
 
-    print "Capture file is '%s'" % capture_file
+    print "Results: %s" % map(lambda c: c['metric'], captures)
+    print "Capture files: %s" % map(lambda c: c['capture_file'], captures)
+
 
 main()
