98571ef7296322e13ecf970acb3f6ce5144d6ddf
William Lachance <wlach@mozilla.com>
2012-04-26 11:08:31 -0400

    Fennec nightly is now at version 15

--- bin/update-dashboard.py
+++ bin/update-dashboard.py
@@ -21,7 +21,7 @@ class NestedDict(dict):
 default_products = [
     {
         "name": "nightly",
-        "url": "http://ftp.mozilla.org/pub/mozilla.org/mobile/nightly/latest-mozilla-central-android/fennec-14.0a1.en-US.android-arm.apk",
+        "url": "http://ftp.mozilla.org/pub/mozilla.org/mobile/nightly/latest-mozilla-central-android/fennec-15.0a1.en-US.android-arm.apk",
         "appname": "org.mozilla.fennec"
     },
     {
