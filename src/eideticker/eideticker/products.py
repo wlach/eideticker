012f18937e18be46ef7bcd4274557a77ad2a3944
William Lachance <wlachance@mozilla.com>
2012-08-10 16:32:30 -0400

    Remove obsolete xul product definition

--- src/eideticker/eideticker/products.py
+++ src/eideticker/eideticker/products.py
@@ -5,11 +5,6 @@ default_products = [
         "appname": "org.mozilla.fennec"
     },
     {
-        "name": "xul",
-        "url": "http://ftp.mozilla.org/pub/mozilla.org/mobile/releases/latest/android/en-US/fennec-10.0.4esr.en-US.android-arm.apk",
-        "appname": "org.mozilla.firefox"
-    },
-    {
         "name": "stock",
         "url": None,
         "appname": "com.android.browser"
