45fa3ff352a56fa2859de0dcb4e6bc0df6325140
William Lachance <wlachance@mozilla.com>
2012-09-13 19:19:14 -0400

    Don't assert if we don't have a SourceStamp in a fennec apk
    That just indicates that it didn't come from a mercurial repository. Not the
    end of the world.

--- src/eideticker/eideticker/apk.py
+++ src/eideticker/eideticker/apk.py
@@ -10,7 +10,11 @@ def get_fennec_appinfo(fname):
     config = ConfigParser.ConfigParser()
     config.readfp(archive.open('application.ini'))
     buildid = config.get('App', 'BuildID')
-    revision = config.get('App', 'SourceStamp')
+    try:
+        revision = config.get('App', 'SourceStamp')
+    except ConfigParser.NoOptionError:
+        # happens if we're not using mercurial to build fennec
+        revision = None
     version = config.get('App', 'Version')
     (year, month, day) = (buildid[0:4], buildid[4:6], buildid[6:8])
     if 'package-name.txt' in archive.namelist():
