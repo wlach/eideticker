a9aa82baf38a95929866f96934c886ef0e7cedd3
William Lachance <wlachance@mozilla.com>
2012-11-06 15:39:48 -0800

    Eideticker module now needs BeautifulSoup

--- src/eideticker/setup.py
+++ src/eideticker/setup.py
@@ -3,7 +3,7 @@ setup(
     name = "eideticker",
     version = "0.1.0",
     packages = find_packages(),
-    install_requires = [ 'mozdevice' ]
+    install_requires = [ 'mozdevice', 'BeautifulSoup' ]
 )
 
 # FIXME: Compile decklink-capture script automatically
