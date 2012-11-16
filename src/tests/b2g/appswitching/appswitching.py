2eaedc5e5d9531542a5e62d8132bf6ce7ffd9b6a
William Lachance <wlachance@mozilla.com>
2012-11-16 15:44:37 -0800

    Bug 812672 - Add a test for app switching
    Tested on the unagi

---
+++ src/tests/b2g/appswitching/appswitching.py
@@ -0,0 +1,27 @@
+from eideticker.test import B2GTest
+import time
+from gaiatest.gaia_test import GaiaApps
+
+class Test(B2GTest):
+
+    def run(self):
+        apps = GaiaApps(self.device.marionette)
+        # open two apps
+        for name in [ 'Calculator', 'Clock', 'Music' ]:
+            app = apps.launch(name)
+            assert app.frame_id is not None
+
+        # switch back to main frame
+        self.device.marionette.switch_to_frame()
+
+        # open cards view
+        self.device.marionette.execute_script("window.dispatchEvent(new Event('holdhome'));")
+        self.start_capture()
+        self.test_started()
+        for i in range(3):
+            for command in [ 'swipe_left', 'swipe_left', 'swipe_right', 'swipe_right' ]:
+                self.device.executeCommand(command, [])
+                time.sleep(0.5)
+        self.test_finished()
+        self.end_capture()
+
