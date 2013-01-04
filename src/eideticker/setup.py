4bb6b212aefd7bbb6c3784c2812bdd46adc0bc92
William Lachance <wlachance@mozilla.com>
2013-01-04 12:29:20 -0500

    Add httplib2 to the list of eideticker dependencies

--- src/eideticker/setup.py
+++ src/eideticker/setup.py
@@ -3,7 +3,7 @@ setup(
     name = "eideticker",
     version = "0.1.0",
     packages = find_packages(),
-    install_requires = [ 'mozdevice', 'BeautifulSoup', 'gaiatest' ]
+    install_requires = [ 'mozdevice', 'BeautifulSoup', 'gaiatest', 'httplib2' ]
 )
 
 # FIXME: Compile decklink-capture script automatically
