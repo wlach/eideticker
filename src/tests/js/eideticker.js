929b15ceff3e077afa147c1ad17dc417e6c532a1
William Lachance <wlachance@mozilla.com>
2013-01-16 14:14:56 -0500

    Use more streamlined, robust logic for starting capture and getting input
    We now wait for the onload event, which means all resources have loaded. Also,
    consolidate all this logic in the eideticker.js file (instead of having to
    manually put it in the test) and remove the requirement that tests import
    our minimal copy of jquery.

--- src/tests/js/eideticker.js
+++ src/tests/js/eideticker.js
@@ -4,6 +4,8 @@ var Eideticker = function () {
   return {
     getParameterByName: function(name) {
       var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+      if (!match)
+        return null;
 
       return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
     },
@@ -29,7 +31,31 @@ var Eideticker = function () {
     },
     finish: function() {
       window.location.href = "/finish.html";
-    }
+    },
+    onload: window.onload
   }
 }();
 
+// bit of a hack, we determine whether or not this is a test by looking for
+// the "testpath" variable, which we assume is only set on the start.html
+// "green screen" page
+if (Eideticker.getParameterByName('testpath') === null) {
+  window.onload = function() {
+    // Run the test, after a brief timeout to make sure the page is *really* loaded and stable
+    setTimeout(function() {
+      if (Eideticker.onload) {
+        Eideticker.onload();
+      }
+
+      var params = "commands=" + Eideticker.getTestType();
+      var http = new XMLHttpRequest();
+      http.open("POST", '/api/captures/input', true);
+      http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      http.setRequestHeader("Content-length", params.length);
+      http.onreadystatechange = function () {
+        if (this.readyState == 4) Eideticker.finish();
+      };
+      http.send(params);
+    }, 1000);
+  };
+}
