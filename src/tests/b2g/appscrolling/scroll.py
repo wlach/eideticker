697d9d8e75b51b544afda8c46f0cfef4abe1dd4c
William Lachance <wlachance@mozilla.com>
2013-04-19 14:02:58 -0400

    Add a b2g test which scrolls through the contacts list

---
+++ src/tests/b2g/appscrolling/scroll.py
@@ -0,0 +1,27 @@
+from eideticker.test import B2GTest
+import time
+from gaiatest.gaia_test import GaiaApps
+
+class Test(B2GTest):
+    def __init__(self, appname=None, **kwargs):
+        super(Test, self).__init__(**kwargs)
+
+        self.appname = appname
+
+    def run(self):
+        apps = GaiaApps(self.device.marionette)
+
+        app = apps.launch(self.appname)
+        assert app.frame_id is not None
+        time.sleep(5)
+        self.start_capture()
+        self.test_started()
+        cmds = []
+        for i in range(100):
+            cmds.append(['scroll_down'])
+        self.device.executeCommands(cmds)
+        self.test_finished()
+        self.end_capture()
+
+        # cleanup: switch back to main frame
+        self.device.marionette.switch_to_frame()
