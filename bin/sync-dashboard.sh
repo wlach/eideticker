c17560c4665506143c4d5929e5b29041b5b10352
William Lachance <wlachance@mozilla.com>
2013-05-03 20:12:52 -0400

    Make it optional which directory on the dashboard server to sync to

--- bin/sync-dashboard.sh
+++ bin/sync-dashboard.sh
@@ -9,7 +9,11 @@ if [ -z $DASHBOARD_SERVER ]; then
     fi
 fi
 
+if [ -z $DASHBOARD_SERVER_DIR ]; then
+    DASHBOARD_SERVER_DIR="~/www/"
+fi
+
 EIDETICKER=$(dirname $0)/../
 
-rsync -avz --copy-links -e ssh $EIDETICKER/src/dashboard/ eideticker@$DASHBOARD_SERVER:~/www/
+rsync -avz --copy-links -e ssh $EIDETICKER/src/dashboard/ eideticker@$DASHBOARD_SERVER:$DASHBOARD_SERVER_DIR
 
