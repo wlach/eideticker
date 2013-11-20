fb7e374c1db4e342ffa3156d0a6bccf78069bd9a
William Lachance <wlachance@mozilla.com>
2013-11-20 13:51:00 -0500

    Bug 939191 - Fix settings scrolling test;r=davehunt

--- src/tests/b2g/appscrolling/settings.py
+++ src/tests/b2g/appscrolling/settings.py
@@ -1,10 +1,22 @@
 from eideticker.test import B2GAppActionTest
+from marionette.by import By
 
 class Test(B2GAppActionTest):
-    def __init__(self, testinfo, appname, **kwargs):
-        B2GAppActionTest.__init__(self, testinfo, appname, **kwargs)
+    def prepare_app(self):
+
+        settings_container = self.device.marionette.find_element(
+            By.CSS_SELECTOR, '#root > div')
+        settings_location = settings_container.location
+        settings_swipe_padding = 16
+
+        scroll_x1 = settings_location['x'] + settings_container.size['width'] / 2
+        scroll_y1 = settings_location['y'] + (settings_container.size['height'] -
+                                              settings_swipe_padding)
+        scroll_y2 = settings_location['y'] + settings_swipe_padding
 
         self.cmds = []
         for i in range(5):
-            self.cmds.append(['scroll_down'])
-            self.cmds.append(['scroll_up'])
+            self.cmds.append(['drag', scroll_x1, scroll_y1, scroll_x1, scroll_y2, 100, 10])
+            self.cmds.append(['sleep', 0.5])
+            self.cmds.append(['drag', scroll_x1, scroll_y2, scroll_x1, scroll_y1, 100, 10])
+            self.cmds.append(['sleep', 0.5])
