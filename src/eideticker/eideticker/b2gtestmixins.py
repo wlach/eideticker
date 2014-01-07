41761026c04f93d6a4ad5a867bbb455a63427576
Dave Hunt <dave.hunt@gmail.com>
2014-01-07 15:13:01 -0500

    Bug 937730 - Do not include pictures when populating contacts;r=wlach

--- src/eideticker/eideticker/b2gtestmixins.py
+++ src/eideticker/eideticker/b2gtestmixins.py
@@ -2,17 +2,17 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import time
-
 from gaiatest.apps.contacts.app import Contacts
 from marionette.errors import NoSuchElementException
 from marionette.errors import ElementNotVisibleException
 from marionette.wait import Wait
 
+
 class B2GContactsTestMixin(object):
 
     def populate_databases(self):
-        self.device.b2gpopulate.populate_contacts(200, restart=False)
+        self.device.b2gpopulate.populate_contacts(
+            200, include_pictures=False, restart=False)
 
     def launch_app(self):
 
--- src/eideticker/setup.py
+++ src/eideticker/setup.py
@@ -3,7 +3,7 @@ setup(
     name = "eideticker",
     version = "0.1.0",
     packages = find_packages(),
-    install_requires = [ 'mozdevice', 'BeautifulSoup', 'gaiatest>=0.21.2', 'httplib2', 'b2gpopulate>=0.10' ]
+    install_requires = [ 'mozdevice', 'BeautifulSoup', 'gaiatest>=0.21.2', 'httplib2', 'b2gpopulate>=0.12' ]
 )
 
 # FIXME: Compile decklink-capture script automatically
