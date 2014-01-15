bbd2e50fd0b8a8c92c9f2b59963cd9e56cbd9404
Dave Hunt <dave.hunt@gmail.com>
2014-01-15 19:25:22 +0000

    Bug 960185 - Fix settings scrolling test. r=wlachance

--- src/tests/b2g/appscrolling/settings.py
+++ src/tests/b2g/appscrolling/settings.py
@@ -1,18 +1,26 @@
-from eideticker.test import B2GAppActionTest
+from gaiatest.apps.settings.app import Settings
 from marionette.by import By
 
-class Test(B2GAppActionTest):
-    def prepare_app(self):
+from eideticker.test import B2GAppActionTest
 
+
+class Test(B2GAppActionTest):
+
+    def launch_app(self):
+        settings = Settings(self.device.marionette)
+        settings.launch()
         settings_container = self.device.marionette.find_element(
             By.CSS_SELECTOR, '#root > div')
+        header = self.device.marionette.find_element(
+            By.CSS_SELECTOR, '#root > header')
         settings_location = settings_container.location
         settings_swipe_padding = 16
 
         scroll_x1 = settings_location['x'] + settings_container.size['width'] / 2
         scroll_y1 = settings_location['y'] + (settings_container.size['height'] -
                                               settings_swipe_padding)
-        scroll_y2 = settings_location['y'] + settings_swipe_padding
+        scroll_y2 = settings_location['y'] + (header.size['height'] +
+                                              settings_swipe_padding)
 
         self.cmds = []
         for i in range(5):
