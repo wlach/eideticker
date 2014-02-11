f970b71b9d6bdc21153bcac1ef348f0f2858323e
William Lachance <wlachance@mozilla.com>
2014-02-11 11:13:17 -0500

    Bug 970151 - Clean gecko profiles before test run on Android;r=gbrown

--- bin/cleanup-phone.py
+++ bin/cleanup-phone.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-import sys
 import eideticker
-
+import re
+import sys
 
 def main(args=sys.argv[1:]):
     usage = "usage: %prog"
@@ -23,5 +23,13 @@ def main(args=sys.argv[1:]):
 
     # clean up any test stuff (profiles, etc.)
     device.removeDir(device.getDeviceRoot())
+
+    # profile files are dumped directly to the sdcard,
+    # not the testroot so we need to clean them up
+    # seperately
+    files = device.listFiles('/mnt/sdcard/')
+    for file in files:
+        if re.match('profile_.*txt', file):
+            device.removeFile('/mnt/sdcard/%s' % file)
 
 main()
