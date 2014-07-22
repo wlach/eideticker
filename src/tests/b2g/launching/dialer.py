fc402f22c7a83997e1c74e3bfdca234d86d25f9b
Gautam Akiwate <gautam.akiwate@gmail.com>
2014-07-22 09:45:41 -0700

    Bug 1035941 - Add test for loading dialer
    Modified test for dialer. The dialer app does not load content
    after launching. Replaced with a no-op for wait_for_content

--- src/tests/b2g/launching/dialer.py
+++ src/tests/b2g/launching/dialer.py
@@ -10,8 +10,7 @@ from gaiatest.apps.phone.app import Phone
 
 class Test(B2GAppStartupTest):
 
+    # The dialer app does not load content after launching.
+    # Thus it is safe to have a no-op for wait_for_content
     def wait_for_content_ready(self):
-        Wait(self.device.marionette).until(lambda m: self.device.gaiaApps.displayed_app.name == 'Phone')
-        self.device.gaiaApps.switch_to_displayed_app()
-        keypad_toolbar_button = self.device.marionette.find_element(*Phone._keypad_toolbar_button_locator)
-        Wait(self.device.marionette).until(lambda m: 'toolbar-option-selected' in keypad_toolbar_button.get_attribute('class'))
+        pass
